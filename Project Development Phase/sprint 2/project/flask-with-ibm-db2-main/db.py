a29ad9947dc511e4a7265bf865a53122abfd8cb4
swethajosh <113223101+swethajosh@users.noreply.github.com>
2022-11-04 00:47:44 +0530

    Add files via upload

---
+++ Project Development Phase/sprint 2/project/flask-with-ibm-db2-main/db.py
@@ -0,0 +1,4 @@
+import ibm_db
+
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=54a2f15b-5c0f-46df-8954-7e38e612c2bd.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32733;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=zlp19068;PWD=6YtcIgY50vs546AM",'','')
+print("CONNECTION SUCCESSFUL")
